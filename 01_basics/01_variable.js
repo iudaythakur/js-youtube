f31bb724943921e99d2d3987566453a67c77c152
iudaythakur <meudaythakurr@gmail.com>
2025-12-28 12:31:04 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountid = 84884
+let accountemail = "hittesh@gmail.com"
+var accountpassword = "34355"
+accontcity = "gumarwin"
+
+let accountstate;
+
+console.table([accountid,accountemail,accountpassword,accountstate])
+
+
+// we avoide the use of the var because the issue of block scope and funtional scope {} these are known as scope
