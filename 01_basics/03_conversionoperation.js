019c8587a866793dab30208787f74efd37207912
iudaythakur <meudaythakurr@gmail.com>
2026-01-01 03:59:48 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+/*let score = "33" */    // any thing written in "" is known as string
+
+//let score = "33aa"  => NaN
+//let score = true    => 1
+//let score = null    => 0
+//let score = undefined => NaN
+let score = "uday"   
+console.log(typeof score)
+// conversion operation
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)       // NaN => Not a Number
+
+//let isloggedin = ""
+let isloggedin = "uday"
+let booleanisloggedin = Boolean(isloggedin)
+console.log(typeof booleanisloggedin)
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "uday" => true
+// null => false
+// undefined => false
+// " " => true
+
+let somestring = 34
+let stringconversion = String(somestring)
+console.log(stringconversion)
+console.log(typeof stringconversion)
