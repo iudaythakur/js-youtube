e4504e69b92317e74576bed1897dd7d0aedd32df
iudaythakur <meudaythakurr@gmail.com>
2026-01-12 07:51:54 +0000

    comparisons , equality

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,22 @@
+// Comparisons and the equality check in the js. 
+
+console.log(2>1)
+console.log(3>=2)
+  
+// when the operators change 
+// the comparison and equality check works differently 
+// reason >= ,<= convert the operators 
+console.log("2">1)
+console.log(1>"2")
+
+console.log(null>0)
+console.log(null == 0);
+console.log(null >= 0); // here conversion check work 
+
+
+//** genrally we avoide the comparison bw the diff operators */
+
+
+console.log(NaN>0);
+console.log(nan>=0);
+
