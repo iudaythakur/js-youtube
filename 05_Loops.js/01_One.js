b29e0b8cf332d588765fd074037e515a43a2ad4d
iudaythakur <meudaythakurr@gmail.com>
2026-01-20 05:43:12 +0000

    Loops

---
+++ 05_Loops.js/01_One.js
@@ -0,0 +1,29 @@
+for (let i = 0; i <= 20; i++) {
+    const element = i;
+  console.log(`${element}`);
+  
+}
+
+
+let myarray = ["Ironman","Thor","Spiderman"]
+
+for (let i = 0; i < myarray.length; i++) {
+    const element = myarray[i];
+    console.log(`${element} You are my fav heros`);
+    
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    console.log(`${element} Outerloop`);
+    for (let j = 1; j <= 10; j++) {
+        const element1 = j;
+        // console.log(`${element} innerloop ${element1}`);
+        console.log(`Multilpication ${element} * ${element1} = ${element * element1}`);
+        
+        }
+        
+    }
+    
+    
---
+++ 05_Loops.js/02_Two.js
@@ -0,0 +1,35 @@
+// While and do While loop
+
+
+
+// let score = 1
+// while (score <= 20) {
+//     // score++;
+//     console.log(`This is the score ${score}`);
+//      score++;
+// }
+
+
+
+let myarray = ["Batman", "Flash", "Wonder Wommen"]
+let arr = 0
+
+while (arr < myarray.length) {
+    // arr = arr + 1
+    console.log(`${myarray[arr]} These are my fav heros`);
+    arr = arr + 1;
+}
+
+
+// let score = 0
+
+// do { score = score + 2
+//     console.log(`${score} This is the score`);
+// } while (score <= 10);
+
+let score = 12
+
+ do { //score = score + 2
+    console.log(`${score} This is the score`);
+    score = score + 2
+} while (score <= 10);
